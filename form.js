58e8a0966c3a49923d292122aff73dc188e82319
Akshay Pandey <117515247+akshaypandey-123@users.noreply.github.com>
2024-06-23 16:25:08 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+ 
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Handle login logic here (validate credentials, etc.)
+  alert('Login button clicked! Implement your login logic here.');
+});
